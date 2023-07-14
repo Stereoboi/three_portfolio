7f93a12bb5407cc431bf9a2152a16bc7e437ff16
Bohdan Pishchak <93521997+Stereoboi@users.noreply.github.com>
2023-07-14 18:11:06 +0300

    add project section

--- src/components/work components/Projects.jsx
+++ src/components/work components/Projects.jsx
@@ -1,3 +1,87 @@
+import projPetlyImage from "../../../public/petly.png";
+import projFootballImage from "../../../public/football.png";
+import projMovieImage from "../../../public/movie.png";
+import { Link } from "react-router-dom";
+import projNodeImage from "../../../public/node.png";
+import {
+  MainWrp,
+  CardWrp,
+  Image,
+  HiddenWrp,
+  Description,
+} from "./Projects.stylrd";
+
 export default function Projects() {
-  return <h1>Projects</h1>;
+  return (
+    <MainWrp>
+      <Link
+        to={"https://tizenmile.github.io/pets-support-project-frontend/"}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <CardWrp>
+          <Image src={projPetlyImage} />
+          <HiddenWrp>
+            <Description>
+              Web app, built using Redux and an Express back-end API, allows you
+              to easily store data about your pets. You can create categories
+              for your pets,and track your pets information.The app stores your
+              data in a secure, personal account, so you can access it from
+              anywhere and at any time.
+            </Description>
+          </HiddenWrp>
+        </CardWrp>
+      </Link>
+      <Link
+        to={"https://github.com/Stereoboi/final-project-backend"}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <CardWrp>
+          <Image src={projNodeImage} />
+          <HiddenWrp>
+            <Description>
+              The API for the "Petly" application is connected to the MongoDB
+              database and allows for user registration and login. It also
+              enables creating, editing, and retrieving filtered lists of posts.
+            </Description>
+          </HiddenWrp>
+        </CardWrp>
+      </Link>
+      <Link
+        to={"https://football-tracker-steel.vercel.app/"}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <CardWrp>
+          <Image src={projFootballImage} />
+          <HiddenWrp>
+            <Description>
+              Football Tracker is a responsive web application built using
+              Next.js 13 andTypeScript, incorporating best practices from the
+              Next.js 13 documentation. It provides a platform for tracking
+              football leagues, teams, news, and player information.
+            </Description>
+          </HiddenWrp>
+        </CardWrp>
+      </Link>
+      <Link
+        to={"https://stereoboi.github.io/CinemaHall/"}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <CardWrp>
+          <Image src={projMovieImage} />
+          <HiddenWrp>
+            <Description>
+              Responsive React app for movie choosing, connected to TMDB API
+              Also you can register or log in by using your Google or Facebook
+              account and make your private collection, data saved on Firestore.
+              Also here implemented private and restricted routs.
+            </Description>
+          </HiddenWrp>
+        </CardWrp>
+      </Link>
+    </MainWrp>
+  );
 }
---
+++ src/components/work components/Projects.stylrd.js
@@ -0,0 +1,46 @@
+import { styled } from "styled-components";
+
+export const MainWrp = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 5px;
+  @media only screen and (max-width: 768px) {
+    padding: 30px;
+  }
+`;
+export const CardWrp = styled.div`
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 5px;
+`;
+export const Image = styled.img`
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+`;
+export const HiddenWrp = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(34, 39, 46, 0.7);
+  opacity: 0;
+  transform: translateY(100%);
+  transition: transform 0.3s ease, opacity 0.3s ease;
+
+  ${CardWrp}:hover & {
+    cursor: pointer;
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+export const Description = styled.p`
+  color: white;
+  font-size: 18px;
+  padding: 10px;
+`;
